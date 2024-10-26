e748c4557f9413853a9184031fa17e93a0570055
nissbar2 <46746305+nissbar2@users.noreply.github.com>
2024-10-26 23:30:09 +0300

    Fix Amplify backend configuration

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "MyApp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "myapp72c2bb37": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3myappstorage5f77d6f4": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
